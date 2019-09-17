051c695c9f2e50006c374b24fc53c2aed68bd519
celtare21 <celtare21@gmail.com>
2019-09-17 22:04:36 +0200

    BoardConfig: Import Dexpreopt config from crosshatch
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,10 +145,12 @@ TARGET_USES_QCOM_DISPLAY_BSP := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+  ifeq ($(WITH_DEXPREOPT),)
+    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_PIC := true
+    ifneq ($(TARGET_BUILD_VARIANT),user)
+      # Retain classes.dex in APK's for non-user builds
+      DEX_PREOPT_DEFAULT := nostripping
     endif
   endif
 endif
