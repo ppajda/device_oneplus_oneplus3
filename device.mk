97f017ce9d32b93fa00cde2e24d6f1f46449f00e
Pajdek <pajdek@gmail.com>
2020-03-19 21:02:25 +0100

    Add OP Apps

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 
+# Camera
+$(call inherit-product, vendor/addons/onepluscamera/config.mk)
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlays
@@ -215,7 +218,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
